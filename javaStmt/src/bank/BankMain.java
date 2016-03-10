2e4977c3daaeee2d187b30d3ec44a1bbeaf01462
kimmiri415 <rlaalfl92@gmail.com>
2016-03-10 16:35:54 +0900

    bank

--- javaStmt/src/bank/BankMain.java
+++ javaStmt/src/bank/BankMain.java
@@ -3,6 +3,8 @@
  */
 package bank;
 
+import java.util.Scanner;
+
 /**
  * @file BankMain.java
  * @author rlaalfl92@gmail.com
@@ -11,5 +13,14 @@ package bank;
  *
  */
 public class BankMain {
+	public static void main(String[] args) {
+		String name = "";
+		int pw;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("이름, password입력하세요");
 
+		AccountBean a = new AccountBean(scanner.next(), scanner.nextInt());
+
+		System.out.println(a.toString());
+	}
 }
--- javaStmt/src/bank/BankServiceImpl.java
+++ javaStmt/src/bank/BankServiceImpl.java
@@ -1,4 +1,63 @@
+/**
+ * 
+ */
 package bank;
+
+/**
+ * @file BankService.java
+ * @author rlaalfl92@gmail.com
+ * @date 2016. 3. 10.
+ * @story 뱅크서비스 비즈니스 로직
+ *
+ */
 public class BankServiceImpl implements BankService {
+
+	@Override
+	public String openAccount(AccountBean acc) {
+		/**
+		 * 계좌 개설
+		 */
+		return null;
+	}
+
+	@Override
+	public AccountBean findAccountByAccountNo(int accountNo) {
+		/**
+		 * 계좌번호로 계좌 조회(반드시 하나만 검색됨)
+		 */
+		return null;
+	}
+
+	@Override
+	public AccountBean[] findAccountsByName(String name) {
+		/**
+		 * 이름으로 계좌 조회(복수개의 결과가능)
+		 */
+		return null;
+	}
+
+	@Override
+	public String closeAccount(int accountNo) {
+		/**
+		 * 계좌 해지
+		 */
+		return null;
+	}
+
+	@Override
+	public int countAll() {
+		/**
+		 * 전체 계좌수 조회
+		 */
+		return 0;
+	}
+
+	@Override
+	public int countByName() {
+		/**
+		 * 해당 이름에 맞는 계좌 수 조회
+		 */
+		return 0;
+	}
 
 }
