27f8c79da9419344d48263d061a7eb3fd9d9f34c
kimmiri415 <rlaalfl92@gmail.com>
2016-03-14 17:50:29 +0900

    추상화

---
+++ javaStmt/src/grade/GradeController.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package grade;
+
+/**
+ * @file GradeController.java
+ * @author rlaalfl92@gmail.com
+ * @date 2016. 3. 14.
+ * @story
+ *
+ */
+public class GradeController {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ javaStmt/src/grade/GradeServiceImpl.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package grade;
+
+/**
+ * @file GradeServiceImpl.java
+ * @author rlaalfl92@gmail.com
+ * @date 2016. 3. 14.
+ * @story
+ *
+ */
+public class GradeServiceImpl {
+
+}
