807d548ed6dd5e3dca1e27441f8cc2f2eb378c37
kimmiri415 <rlaalfl92@gmail.com>
2016-03-15 18:26:09 +0900

    HashMap member패키지 완성

--- javaStmt/src/member/MemberServiceImpl.java
+++ javaStmt/src/member/MemberServiceImpl.java
@@ -36,9 +36,8 @@ public class MemberServiceImpl implements MemberService {
 		// 아이디가 존재하지 않아서 실패한 경우와 비번이 틀려서 실패한 경우에 따라서
 		// 메시지를 전달해줘야함
 
-		return (map.containsKey(id))
-				? ((map.get(id).getPassword() == password) ? map.get(id).getName() + "님 로그인하셨습니다." : "비밀번호를 확인해주세요.")
-				: "존재하지 않는 id입니다.";
+		return ((map.containsKey(id)) ? ((map.get(id).getPassword().equals(password))
+				? map.get(id).getName() + "님 로그인하셨습니다." : "비밀번호를 확인해주세요.") : "존재하지않는 id입니다.");
 
 	}
 
