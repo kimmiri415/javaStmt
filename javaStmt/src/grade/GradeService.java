3297d638ad4bcc77bcc42a6a11ae6f14a13d7eaf
kimmiri415 <rlaalfl92@gmail.com>
2016-03-15 16:45:36 +0900

    Grade 완료

--- javaStmt/src/grade/GradeService.java
+++ javaStmt/src/grade/GradeService.java
@@ -1,6 +1,6 @@
 package grade;
 
-import java.util.Vector;
+import java.util.ArrayList;
 
 /**
  * CRUD C : create 생성 R : read 조회 U : update 수정 D : delete 삭제
@@ -8,26 +8,26 @@ import java.util.Vector;
 public interface GradeService {
 	// C 성적표 등록
 	public void input(GradeBean grade);// 추상 메소드
-	
+
 	// R 성적표 리스트 출력
-	public Vector<GradeBean> getList();
-	
+	public ArrayList<GradeBean> getList();
+
 	// R 성적표 조회(학번)
-	public void getGradeByHak();
-	
+	public GradeBean getGradeByHak(int hak);
+
 	// R 성적표 조회(이름)
-	public void getGradesByName();
-	
+	public ArrayList<GradeBean> getGradesByName(String name);
+
 	// R 카운트 조회
-	public void getCount();
-	
+	public int getCount();
+
 	// R 이름조회시 카운트조회
 	public void getCountByName();
 
 	// U 성적표 수정
-	public void update();
+	public String update(GradeBean grade);
 
 	// D 성적표 삭제
-	public void delete();
+	public String delete(int hak);
 
 }
--- javaStmt/src/grade/GradeServiceImpl.java
+++ javaStmt/src/grade/GradeServiceImpl.java
@@ -1,40 +1,60 @@
 //미리언니 자리
 package grade;
 
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class GradeServiceImpl implements GradeService {
 	// 멤버필드
-	Vector<GradeBean> gradeList;
+	ArrayList<GradeBean> gradeList;
 
 	public GradeServiceImpl() {
-		gradeList = new Vector<GradeBean>();
+		gradeList = new ArrayList<GradeBean>();
 	}
 
 	// 멤버 메소드 에어리어
 	public void input(GradeBean grade) {
 		// C 성적표 등록
+		gradeList.add(new GradeBean(2, "김구", 100, 100, 100, 100));
+		gradeList.add(new GradeBean(3, "강감찬", 90, 90, 90, 90));
+		gradeList.add(new GradeBean(4, "유관순", 80, 80, 80, 80));
+		gradeList.add(new GradeBean(5, "김구", 70, 70, 70, 70));
 		gradeList.add(grade);
 	}
 
-	public Vector<GradeBean> getList() {
-		// R 성적표 리스트 출력	
+	public ArrayList<GradeBean> getList() {
+		// R 성적표 리스트 출력
 		return gradeList;
 	}
 
-	public void getGradeByHak() {
+	public GradeBean getGradeByHak(int hak) {
 		// R 성적표 조회(학번)
+		GradeBean tempGrade = new GradeBean();
+		for (int i = 0; i < gradeList.size(); i++) {
 
+			if (hak == gradeList.get(i).getHak()) {
+				tempGrade = gradeList.get(i);
+				break;
+			}
+		}
+		return tempGrade;
 	}
 
 	@Override
-	public void getGradesByName() {
+	public ArrayList<GradeBean> getGradesByName(String name) {
 		// R 성적표 조회(이름)
+		ArrayList<GradeBean> tempList = new ArrayList<GradeBean>();
+		for (int i = 0; i < gradeList.size(); i++) {
+			if (name.equals(gradeList.get(i).getName())) {
+				tempList.add(gradeList.get(i));
 
+			}
+		}
+		return tempList;
 	}
 
-	public void getCount() {
+	public int getCount() {
 		// R 카운트 조회
+		return gradeList.size();
 
 	}
 
@@ -43,14 +63,28 @@ public class GradeServiceImpl implements GradeService {
 
 	}
 
-	public void update() {
+	public String update(GradeBean grade) {
 		// U 성적표 수정
+		String result = "수정하려는 학번이 존재하지 않음";
+		if (gradeList.contains(getGradeByHak(grade.getHak()))) {
+			GradeBean searchedGrade = getGradeByHak(grade.getHak());
+			searchedGrade.setJava(grade.getJava());
+			searchedGrade.setSql(grade.getSql());
+			searchedGrade.setJsp(grade.getJsp());
+			searchedGrade.setSpring(grade.getSpring());
 
+			result = "수정 성공 " + searchedGrade;
+			// delete(grade.getHak());
+			// input(grade);
+		}
+
+		return result;
 	}
 
-	public void delete() {
+	public String delete(int hak) {
 		// D 성적표 삭제
 
+		return ((gradeList.remove(getGradeByHak(hak))) ? "삭제성공" : "삭제실패");
 	}
 
 }
